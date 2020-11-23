fc3097033824236ee2b053ac752a0fc0885f9073
prakashsvmx <prakash@minio.io>
2020-11-23 13:35:09 +0530

    Statements and assignment

--- hello.go
+++ hello.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World...!")
+	fmt.Printf("hello, world\n")
+
+	nBytes, _ := fmt.Println("Hello world..")
+	fmt.Println(nBytes,)
+	x:=10
+	fmt.Println(x)
+	if x > 5 {
+		fmt.Println("Great..")
+	}
+
+	if(x+5 <10){
+		fmt.Println("Lesser")
+	} else{
+		fmt.Println("LOt better")
+	}
+
 }
